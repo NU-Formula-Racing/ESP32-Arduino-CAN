9292b28b12364cf4af2d39242536203e1eaca81d
feistjo <joshua.feist@gmail.com>
2022-10-01 15:01:56 -0500

    Added timeout

--- src/ESP32CAN.cpp
+++ src/ESP32CAN.cpp
@@ -4,15 +4,15 @@ int ESP32CAN::CANInit()
 {
     return CAN_init();
 }
-int ESP32CAN::CANWriteFrame(const CAN_frame_t* p_frame)
+int ESP32CAN::CANWriteFrame(const CAN_frame_t *p_frame, TickType_t timeout)
 {
-    return CAN_write_frame(p_frame);
+    return CAN_write_frame(p_frame, timeout);
 }
 int ESP32CAN::CANStop()
 {
     return CAN_stop();
 }
-int ESP32CAN::CANConfigFilter(const CAN_filter_t* p_filter)
+int ESP32CAN::CANConfigFilter(const CAN_filter_t *p_filter)
 {
     return CAN_config_filter(p_filter);
 }
--- src/ESP32CAN.h
+++ src/ESP32CAN.h
@@ -6,11 +6,11 @@
 
 class ESP32CAN
 {
-    public: 
-        int CANInit();
-		int CANConfigFilter(const CAN_filter_t* p_filter);
-        int CANWriteFrame(const CAN_frame_t* p_frame);
-        int CANStop();
+public:
+    int CANInit();
+    int CANConfigFilter(const CAN_filter_t *p_filter);
+    int CANWriteFrame(const CAN_frame_t *p_frame, TickType_t timeout = portMAX_DELAY);
+    int CANStop();
 };
 
 extern ESP32CAN ESP32Can;
